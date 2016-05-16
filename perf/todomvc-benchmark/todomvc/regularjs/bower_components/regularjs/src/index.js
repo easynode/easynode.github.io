421a399c614c0bfafcaea591cf4be77bb3841be3
胡家宝 <jiabao_hu@corp.netease.com>
2016-05-16 21:32:31 +0800

    remove regular

--- perf/todomvc-benchmark/todomvc/regularjs/bower_components/regularjs/src/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = require("./Regular.js");
-require("./directive/base.js");
-require("./module/timeout.js");
-module.exports.dom = require("./dom.js");
-module.exports.util = require("./util.js");
-
